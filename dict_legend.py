86b98ecf52ab1f6ac2bc6c5ba23056275f308deb
Oskar Bäcklin <oskar.backlin@gu.se>
2020-06-29 15:18:31 +0200

    Create dict_legend.py

---
+++ dict_legend.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# a dict for better display variable names
+dict_var_disp = {
+    
+    'Kdown': r'$K_{\downarrow}$',
+    'Kup': r'$K_{\uparrow}$',
+    'Ldown': r'$L_{\downarrow}$',
+    'Lup': r'$L_{\uparrow}$',
+    
+    'QN': '$Q^*$',
+    'QS': '$Q_S$',
+    'QE': '$Q_E$',
+    'QH': '$Q_H$',
+    'QF': '$Q_F$',
+    'RH2':'RH',
+    'T2': '$T_2$',
+    'Q2': '$Q_2$',
+    'UStar':'$U^*$',
+    'SMD':'SMD',
+    #_r
+    'QN_r': '$Q^*$_r',
+    'QS_r': '$Q_S$_r',
+    'QE_r': '$Q_E$_r',
+    'QH_r': '$Q_H$_r',
+    'QF_r': '$Q_F$_r',
+    'RH2_r':'RH_r',
+    'T2_r': '$T_2$_r',
+    'Q2_r': '$Q_2$_r',
+    'UStar_r':'$U^*$_r',
+    'SMD_r':'SMD_r',
+    #_rb
+    'QN_rb': '$Q^*$_rb',
+    'QS_rb': '$Q_S$_rb',
+    'QE_rb': '$Q_E$_rb',
+    'QH_rb': '$Q_H$_rb',
+    'QF_rb': '$Q_F$_rb',
+    'RH2_rb':'RH_rb',
+    'T2_rb': '$T_2$_rb',
+    'Q2_rb': '$Q_2$_rb',
+    'UStar_rb':'$U^*$_rb',
+    'SMD_rb':'SMD_rb',
+    #_delta_r
+    'QN_r_d': '$\\Delta Q^*$_r',
+    'QS_r_d': '$\Delta Q_S$_r',
+    'QE_r_d': '$\Delta Q_E$_r',
+    'QH_r_d': '$\Delta Q_H$_r',
+    'QF_r_d': '$\Delta Q_F$_r',
+    'RH2_r_d':'$\\Delta RH$_r',
+    'T2_r_d': '$\\Delta T_2$_r',
+    'Q2_r_d': '$\\Delta Q_2$_r',
+    'UStar_r_d':'$\\Delta U^*$_r',
+    'U10_r_d':'$\\Delta U10$_r',
+    'SMD_r_d':'$\\Delta SMD$_r',        
+    'RA_r_d':'$\\Delta RA$_r',
+
+    #_delta_rb
+    'QN_rb_d': '$\\Delta Q^*$_rb',
+    'QS_rb_d': '$\Delta Q_S$_rb',
+    'QE_rb_d': '$\Delta Q_E$_rb',
+    'QH_rb_d': '$\Delta Q_H$_rb',
+    'QF_rb_d': '$\Delta Q_F$_rb',
+    'RH2_rb_d':'$\\Delta RH$_rb',
+    'T2_rb_d': '$\\Delta T_2$_rb',
+    'Q2_rb_d': '$\\Delta Q_2$_rb',
+    'UStar_rb_d':'$\\Delta U^*$_rb',
+    'U10_rb_d':'$\\Delta U10$_rb',
+    'SMD_rb_d':'$\\Delta SMD$_rb',
+    'RA_rb_d':'$\\Delta RA$_rb',
+
+}
+
+list_var_plot = [
+    'QN',
+    'QS',
+    'QE',
+    'QH',
+    'QF',
+    'RH2',
+    'T2'
+]
+dict_var_label = {
+    'QN': 'Flux ($ \mathrm{W \ m^{-2}}$)',
+    'QS': 'Flux ($ \mathrm{W \ m^{-2}}$)',
+    'QE': 'Flux ($ \mathrm{W \ m^{-2}}$)',
+    'QH': 'Flux ($ \mathrm{W \ m^{-2}}$)',
+    'QF': 'Flux ($ \mathrm{W \ m^{-2}}$)',
+    'RH2': 'Relative Humidity (%)',
+    'T2': 'Air Temperature\n2 magl ($^{\\circ}}$C)',
+}
+
+
+
+
